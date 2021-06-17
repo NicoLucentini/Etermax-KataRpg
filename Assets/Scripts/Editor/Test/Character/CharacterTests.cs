fc0fcfc4622cc71b3eacf9fca2312852d350b039
Nicolas Lucentini <lucentini.nicolas@gmail.com>
2021-06-16 22:51:49 -0300

    Se agrega test del personaje

---
+++ Assets/Scripts/Editor/Test/Character/CharacterTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using com.rpg.domain;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Editor.Test.Actions
+{
+    public class CharacterTests
+    {
+
+
+        [Test]
+        public void CharacterSetupsCorrectly()
+        {
+            Character actor = new Character("Actor");
+
+            Assert.AreEqual(Character.MAX_HEALTH, actor.Health);
+            Assert.AreEqual(1, actor.Level);
+            Assert.True(actor.Alive);
+        }
+
+        
+    }
+}
